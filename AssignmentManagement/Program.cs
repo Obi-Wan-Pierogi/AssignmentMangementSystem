5e5ae4b0dd993af03b316159fbb8e836b9605e3b
Lee Houk <leehouk@gmail.com>
2025-04-27 15:05:07 -0700

    Add project files.

---
+++ AssignmentManagement/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AssignmentManagement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
